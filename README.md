cbf1238ee6b5d11e99a8d4bef3f9abe4fcda7b4e
Scott MacDonald <scott.macdonald@brandhealth.ca>
2014-06-10 17:50:59 -0400

    Added comments and ability to set default elements to show

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ simple-accordion
 ================
 
 A simple, performance efficient, and semantic collapsable accordion jQuery plugin that degrades gracefully for users without javascript.
+
+Usage
+-----
+
+$.accordion();
+$.accordion('#panel-1');
+$('.accordion').accordion();
+$('.accordion').accordion('#panel-1');
--- accordion.jquery.js
+++ accordion.jquery.js
@@ -1,22 +1,24 @@
 ;(function($) {
-    function accordion() {
+    function accordion(defaultTab) {
         'use strict';
 
-        var accordion = $(this);
-        var hash = window.location.hash;
-        var useHash = hash.length > 0;
-        var hashEl;
-
+        // Gives the select panel and it's title the 'active' class
         function giveActiveClass(el) {
             el.height(el.data('h'));
             el.add(el.prev('dt')).addClass('active');
         }
 
+        // Removes 'active' class from all sibling elements and sets it to current active element if applicable
         function setActiveSlide(e) {
+
+            // Safety net to prevent hijacking normal links
             if (e && !$(this).parent().is('dt')) return;
 
-            var active = accordion.children($(this).attr('href'));
+            // Get target panel and remove active class from all siblings
+            var active = $(this).parent('dt').siblings($(this).attr('href'));
             active.siblings('.active').removeClass('active');
+
+            // If we clicked the currently open panel, toggle it shut, else close siblings and activate current panel
             if (active.height() > 1) {
                 active.height(0);
             } else {
@@ -25,30 +27,49 @@
             }
         }
 
-        // Init
-        accordion.children('dd').each(function() {
-            $(this).height('auto').data('h', $(this).height());
+        // Run for each individual definition list
+        $(this).each(function() {
 
-            if (useHash && $(this).is(hash)) {
-                hashEl = $(this);
-                giveActiveClass(hashEl);
-            } else if(!useHash && $(this).data('default') !== undefined) {
-                giveActiveClass($(this));
-            } else {
-                $(this).height(0);
-            }
+            // If this isn't a definition list exit early
+            if ($(this).prop('tagName').toLowerCase() !== 'dl') return;
+
+            var hash = window.location.hash;
+            var tab = defaultTab || $(this).data('accordion');
+            var useHash = hash.length > 0 && $(this).has(hash).length > 0;
+
+            // Iterate each child to decide if it should be active or not, and store height information
+            $(this).children('dd').each(function() {
+                var hashMatches = useHash && $(this).is(hash);
+                var isDefault = !useHash && $(this).is(tab);
+
+                // Temporarily switch to auto height to get length with content,
+                // the element gets re-hidden where appropriate without ever getting drawn to the screen
+                $(this).height('auto').data('h', $(this).height());
+
+                // Set panel as active if applicable
+                if (hashMatches || isDefault) {
+                    giveActiveClass($(this));
+                } else {
+                    $(this).height(0);
+                }
+
+            });
         });
 
-        // Bind
-        accordion.on('click', 'a', setActiveSlide);
+        // Bind clicks
+        $(this).on('click', 'a', setActiveSlide);
 
+        // Return jQuery object
         return this;
     }
 
-    function selfInit() {
-        return $('dl[data-accordion]').accordion();
+    // Alternate init method that initiates on all definition lists with the accordion class
+    function selfInit(defaultTab) {
+        return $('dl.accordion').accordion(defaultTab);
     }
 
+    // Actually extend jQuery here
     $.fn.accordion = accordion;
     $.accordion = selfInit;
+
 })(jQuery);
